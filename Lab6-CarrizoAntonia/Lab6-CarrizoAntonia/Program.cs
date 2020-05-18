cddca8f3dab144d3c5b7e2103958b3d663bbfd55
Antonia Andrea Carrizo Berroeta <antoocb@MacBook-Air-de-Antonia.local>
2020-05-18 02:08:22 -0400

    gitignore

---
+++ Lab6-CarrizoAntonia/Lab6-CarrizoAntonia/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab6CarrizoAntonia
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
